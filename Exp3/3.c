1a6687286f45e1fb1983a52c37cdcfb33cbf97cc
Tresnor <17co28@aitdgoa.edu.in>
2019-03-07 10:26:56 +0530

    exp3 complete

--- Exp3/3.c
+++ Exp3/3.c
@@ -1,34 +1,40 @@
 #include<stdio.h>
-void addition();
-void subtraction();
+int addition(int , int);
+void subtraction(int, int);
 int main()
 {
- int n;
+ int n,num1,num2,i,p;
  printf("1-Addition\n2-Subtraction: ");
  scanf("%d",&n);
  switch(n)
  {
-  case 1 : addition();
-           break;
-  case 2 : subtraction();
-           break;
+  case 1 :  printf("Enter the binary numbers:\n ");
+            printf("First number : ");
+            scanf("%d",&num1);
+            printf("\nSecond number : ");
+            scanf("%d",&num2);
+            p=addition(num1,num2);
+            printf("The addition is: %d", p);
+            break;
+  case 2 :  printf("Enter the binary numbers:\n ");
+            printf("First number : ");
+            scanf("%d",&num1);
+            printf("\nSecond number : ");
+            scanf("%d",&num2);
+            subtraction(num1,num2);
+            break;
   default : printf("\nError\n");
 
 }
 }
 
-void addition()
+int addition(int n1, int n2)
 {
-   int num1,num2,dummy1,dummy2,k,i=7,f,a,b,c;
-   int add[]={0,0,0,0,0,0,0,0};
-   printf("Enter the binary numbers:\n ");
-   printf("First number : ");
-   scanf("%d",&num1);
-   printf("\nSecond number : ");
-   scanf("%d",&num2);
+   int dummy1,dummy2,k,f,a,b,c,j,t=0,i=7;
+   static int add[]={0,0,0,0,0,0,0,0};
 
-   dummy1=num1;
-   dummy2=num2;
+   dummy1=n1;
+   dummy2=n2;
    c=0;
    while((dummy1!=0)&&(dummy2!=0)||i>0)
    {
@@ -58,104 +64,44 @@ void addition()
       dummy2=dummy2/10;
 }
       printf("\n");
-      for(i=0;i<8;i++)
+      for(j=0;j<8;j++)
       {
-          printf("%d ",add[i]);
+        t=t*10+add[j];
       }
+      return t;
 }
 
-void subtraction()
+void subtraction(int n1, int n2)
 {
-    int num1,num2,dummy1,dummy2;
-    int i=7,j=7,k=8,f;
-    int a,b,c;
-    int sub[]={0,0,0,0,0,0,0,0},v[]={0,0,0,0,0,0,0,0},p[]={1,1,1,1,1,1,1,1};
-    printf("Enter the binary numbers which you want to subtract\n");
-    printf("Enter the first number : ");
-    scanf("%d",&num1);
-    printf("\nEnter the second number : ");
-    scanf("%d",&num2);
-    dummy1=num1;
-    dummy2=num2;
-    printf("\nERROR IN 1");
-    while(dummy1!=0)
+    int dummy1,dummy2,dummy4,ans,temp,j,i=8;
+    int dummy3[]={0,0,0,0,0,0,0,0};
+    dummy1=n1;
+    dummy2=n2;
+
+    while(dummy2>0)
     {
-       printf("\nERROR IN 2");
-       a=dummy1%10;
-       v[i]=a;
-       i=i-1;
-       dummy1=dummy1/10;
-    }
-    while(dummy2!=0)
-    {
-       printf("\nERROR IN 3 ");
-       b=dummy2%10;
-    if(b==1)
-    {
-       p[j]=b;
-       i=i-1;
-       dummy2=dummy2/10;
-    while(dummy2!=0)
-    {
-       printf("\nERROR IN 4");
-       a=dummy2%10;
-       if(a==0)
-       {
-          p[j]=1;
-          i=i-1;
-          dummy2=dummy2/10;
-       }
-       if(a==1)
-       {
-          p[j]=0;
-          i=i-1;
-       dummy2=dummy2/10;
-       }
-    }
- }
-    printf("\nERROR what");
-    p[j]=b;
-    i=i-1;
-    dummy2=dummy2/10;
+        i--;
+        temp=dummy2%10;
+        dummy2=dummy2/10;
+        dummy3[i]=temp;
     }
 
-    dummy1=num1;
-    dummy2=num2;
-    c=0;
-    printf("\nERROR IN SUBTRACTION");
-    while((dummy1!=0)&&(dummy2!=0)||i>0)
+    for(i=0;i<8;i++)
     {
-       i=7,j=7,k=7;
-       a=v[k];
-       b=p[j];
-       f=a+b+c;
-       switch(f)
-       {
-          case 0 : sub[i]=f;
-                   c=0;
-                   i=i-1;
-                   break;
-          case 1 : sub[i]=f;
-                   c=0;
-                   i=i-1;
-                   break;
-          case 2 : sub[i]=0;
-                   c=1;
-                   i=i-1;
-                   break;
-          case 3 : sub[i]=1;
-                   c=1;
-                   i=i-1;
-                   break;
-     }
-     k=k-1;
-     j=j-1;
-     dummy1=dummy1/10;
-     dummy2=dummy2/10;
-}
+        if(dummy3[i]==0)
+        dummy3[i]=1;
+        else
+        dummy3[i]=0;
+    }
 
-   for(i=0;i<8;i++)
-   {
-      printf("%d",sub[i]);
-   }
+    for(j=0;j<8;j++)
+    {
+        temp=temp*10+dummy3[j];
+    }
+
+    dummy4=addition(temp,1);
+
+    ans=addition(dummy1,dummy4);
+
+    printf("The subtraction is: %d",ans);
 }
